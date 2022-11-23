2c0e7a86a1f5e4013e16e4e0dd815ac9cf634eb3
Tony Ruiz <tonyruizo@pm.me>
2022-11-22 21:27:56 -0500

    Kata Complete

---
+++ Javascript/multiply.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript?collection=lesson-5-practice-challenges-number-fullstackroadmap
+
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// It was missing the "return"
